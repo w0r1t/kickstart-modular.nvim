818efe85f6124e05aebe9cc48b55355a47c6dc81
Kirill Egorov <ke.w0r1t@gmail.com>
2024-10-26 11:05:20 +0700

    nvim-tree plugin & toggle keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -31,6 +31,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Tree keymaps
+vim.keymap.set('n', '<leader>t', '<cmd>:NvimTreeToggle<CR>', { desc = 'Toggle tree' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
